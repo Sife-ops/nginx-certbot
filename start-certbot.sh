5d57f7f82d35c9f0ccd288e4cfb06bdfa4a84cb6
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-02-18 23:03:00 -0800

    compose variables

--- start-certbot.sh
+++ start-certbot.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-if ! touch ./dhparam/dhparam-2048 ; then
+. ./.env
+
+if ! readlink -f ./dhparam/dhparam-2048 ; then
     mkdir ./dhparam
     openssl dhparam -out ./dhparam/dhparam-2048.pem 2048
 fi
+
+sed "/server_name/ s/<++>/${CERTBOT_DOMAIN}/" ./nginx-template/default.conf > ./nginx-certbot/default.conf
 
 docker-compose -f ./docker-compose-certbot.yml up
