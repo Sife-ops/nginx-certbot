3ff445c8ab27643143fcdf382e4cf0cac55b42b4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-02-24 23:08:15 -0800

    refactor deploy scripts

--- 10-proxy.sh
+++ 10-proxy.sh
@@ -4,4 +4,4 @@
 
 sed "s/<++>/${CERTBOT_DOMAIN}/" ./nginx-template/default.conf > ./nginx/default.conf
 
-docker-compose -f ./docker-compose.yml up ${1}
+docker-compose -f ./docker-compose.yml up $@
--- env.sh
+++ env.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if ! stat ./.env 1>/dev/null 2>&1 ; then
-    echo 'Error: no .env file'
-    exit 1
-fi
+if stat ./.env 1>/dev/null 2>&1 ; then . ./.env ; fi
 
-. ./.env
+if [ -z $CERTBOT_EMAIL ] ; then fail=1 ; fi
+if [ -z $CERTBOT_DOMAIN ] ; then fail=1 ; fi
+
+if [ -n $fail ] ; then 
+    echo 'Error: environment variables'
+    exit 1 
+fi
 
